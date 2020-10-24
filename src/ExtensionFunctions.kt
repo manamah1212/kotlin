9f458cbefb806c1f5d4b6847a202083bc6107ecd
jevgenijs.greibo <manamah1212@gmail.com>
2020-10-24 11:39:18 +0300

    1par done

---
+++ src/ExtensionFunctions.kt
@@ -0,0 +1,5 @@
+
+
+fun String.addEURSign(): String{
+    return "$this €"
+}
--- src/HelloKotlin.kt
+++ src/HelloKotlin.kt
@@ -1,4 +1,17 @@
+import kotlin.math.pow
+
 fun main() {
+    // println(functionWithParamsAndReturn(23))
+    //nullability()
+//MyCar("BMW").outputMyCar()
+//var myNewCaar = MyPresonalCar("BMW",2015)
+//var Kristaps = Human(70.7,23,1.88,"green")
+//println("Hey my weight is ${Kristaps.weight},I am ${Kristaps.age}, and i have ${Kristaps.eyeColor} eyes ")
+
+//debugging()
+//functionWithParamsAndReturn(55)
+    extensionFunctions()
+}
     fun arrays() {
         //Array decloration
         //Array have fixed allocated memory
@@ -29,5 +42,112 @@ fun main() {
             println(it)
         }
 
+
     }
+
+
+fun functionWithParametrs(age: Int) {
+    println("My age is $age")
+}
+
+fun functionWithReturn(): Boolean {
+    return false
+}
+
+//takes @param [age]
+//return birth year
+
+fun functionWithParamsAndReturn(age: Int): Int {
+    return 2020 - age
+}
+
+fun nullability() {
+
+    //we can create null var but we have to add?
+    val text: String? = "null"
+
+    //1. safe call(?.)
+    //returns the lenght if  'text' is not null else return null
+    // use it if you dont mind getting null value
+    //  println("The lenght of name is ${text?.length}")
+
+    //.2 safe call with let(?.let)
+    //it executes the block ONLY IF NAME is NOT null
+//    text?.let {
+//        println("Text is not null, its $it")
+//    }
+
+    //3. Elvis - operator
+    //we have nullable reference 'text' we can say īs name is not null'' use it
+    //otherwise use some non-null value
+
+//    val variable = if (text!= null){
+//        text.length
+//    }else {
+//        -1
+//    }
+
+//    val variable2  = text?.length?: -1
+//    println (variable2)
+
+    //4. non-null assertation operator
+//use it when you are sure the VALUE IS NOT NULL
+// Throws NullPointerException if the
+    println("the lenght of name is${text!!.length}")
+}
+
+//simple class with simple method
+
+class Car {
+    private val carModel = "Lada"
+    fun outputCar() {
+        println("my car is $carModel")
+    }
+}
+
+//simple class with primary constructor
+
+class MyCar(name: String){
+    val myCar = name
+    fun outputMyCar(){
+        println("This is my car: $myCar")
+    }
+}
+class MyPresonalCar(val myCar: String, var myCarYear: Int){
+
+    init {
+       println("My car is $myCar and age is ${2020-myCarYear}")
+    }
+
+}
+
+data class Human(val weight: Double, val age: Int, val height: Double,val eyeColor: String){
+//    val weight : Double,
+//    val age: Int,
+//    val height: Double,
+//    val eyeColor: String
+}
+
+fun debugging() {
+
+    val a = 100.98
+    val b = 105.23
+
+    val c = a.pow(2)/ b
+
+    val d = false
+    val e = !d
+
+    val f = "Kristaps"
+    val g = "My name is $f"
+
+    val h = arrayOf(1, 2, 3, 4, 5, 6, 7)
+    val i = h[6]
+
+    print(a)
+
+}
+
+fun extensionFunctions(){
+println("1000".addEURSign())
 }
